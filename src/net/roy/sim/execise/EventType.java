88bc6a167646ad8f2bdfce2a6159a9c938970906
Roy <royqh1979@gmail.com>
2014-12-26 23:05:37 +0800

    * initial commit

---
+++ src/net/roy/sim/execise/EventType.java
@@ -0,0 +1,10 @@
+package net.roy.sim.execise;
+
+/**
+ * Created by Roy on 2014/12/26.
+ */
+public enum EventType {
+    CustomerArrival,
+    CustomerDepart,
+    Unknown
+}
---
+++ src/net/roy/sim/execise/ServerStatus.java
@@ -0,0 +1,9 @@
+package net.roy.sim.execise;
+
+/**
+ * Created by Roy on 2014/12/26.
+ */
+public enum ServerStatus {
+    Busy,
+    Idle
+}
