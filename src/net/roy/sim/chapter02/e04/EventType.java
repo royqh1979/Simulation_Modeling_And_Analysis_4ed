2fbd5f218d6720ed24cea8f8695fd26ac5647026
Roy <royqh1979@gmail.com>
2015-01-01 23:59:02 +0800

    * add chapter 2.6 , multiteller bank with jockeying, not finished yet.

---
+++ src/net/roy/sim/chapter02/e04/EventType.java
@@ -0,0 +1,9 @@
+package net.roy.sim.chapter02.e04;
+
+/**
+ * Created by Roy on 2015/1/1.
+ */
+public enum EventType {
+    Arrival,
+    Departure
+}
---
+++ src/net/roy/sim/chapter02/e04/Simulator.java
@@ -0,0 +1,17 @@
+package net.roy.sim.chapter02.e04;
+
+/**
+ * a multiteller bank with jockeying
+ * chapter 2.6
+ * Created by Roy on 2015/1/1.
+ */
+public class Simulator {
+    /* parameters */
+    private double doorCloseTime;
+    private double meanInterarrivalTime;
+    private double meanServiceTime;
+    /* parameters change at each simulation */
+    private int numOfTellers;
+
+
+}
